1379c0e3d71e2b84a95ff541c5c3d3c22f6ed791
Witt997 <106078083+Witt997@users.noreply.github.com>
2023-11-05 21:11:25 +0100

    Add files via upload

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,53 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for k, v in kwargs.items():
+            self.contents += v*[k] 
+            # to create a list with keys multiplied by values
+        #print(self.contents)
+
+    def draw(self, num):
+        try:
+            balls = random.sample(self.contents, num)
+        except:
+            balls = copy.deepcopy(self.contents)
+
+        #print(balls)
+        for ball in balls:
+            self.contents.remove(ball)
+        return balls
+
+
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    M = 0
+    for i in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        balls = hat_copy.draw(num_balls_drawn)
+        # check if balls match expectations
+        eb_list = []
+        for k,v in expected_balls.items():
+            eb_list += v*[k]        
+
+        #if all(x in balls for x in eb_list):
+        if contains_balls(eb_list, balls):
+            M += 1
+
+    probability = M/num_experiments
+    return probability
+
+def contains_balls(expected_balls, actual_balls):
+    for b in expected_balls:
+        if b in actual_balls:
+            actual_balls.remove(b)
+        else:
+            return False
+    return True
+
+    
+
+
+
